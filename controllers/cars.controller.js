3f644c9b2fa904375adc026a6d34971d18009b06
mquispe96 <mquispe@pursuit.org>
2024-07-17 19:57:16 -0400

    added hump library to switch between JS and SQL key syntax

--- controllers/cars.controller.js
+++ controllers/cars.controller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const humps = require('humps');
 const cars = express.Router();
 const {
   getAllCars,
@@ -16,7 +17,8 @@ const {
 cars.get('/', async (req, res) => {
   const allCars = await getAllCars();
   if (allCars[0]) {
-    res.status(200).json(allCars);
+    const jsObj = humps.camelizeKeys(allCars);
+    res.status(200).json(jsObj);
   } else {
     res.status(404).json({error: 'No cars found'});
   }
@@ -25,7 +27,8 @@ cars.get('/', async (req, res) => {
 cars.get('/:id', async (req, res) => {
   const car = await getCar(req.params.id);
   if (car.id) {
-    res.status(200).json(car);
+    const jsObj = humps.camelizeKeys(car);
+    res.status(200).json(jsObj);
   } else {
     res.status(404).json({error: 'Car not found'});
   }
@@ -36,12 +39,14 @@ cars.post(
   hasAllRequiredFields,
   validateFieldsDataTypes,
   async (req, res) => {
-    const formattedBody = formatBody(req.body);
+    const sqlObj = humps.decamelizeKeys(req.body);
+    const formattedBody = formatBody(sqlObj);
     const car = await createCar(formattedBody);
     if (!car.id) {
       res.status(400).json(car.error);
     } else {
-      res.status(201).json(car);
+      const jsObj = humps.camelizeKeys(car);
+      res.status(201).json(jsObj);
     }
   },
 );
@@ -51,12 +56,14 @@ cars.put(
   hasAllRequiredFields,
   validateFieldsDataTypes,
   async (req, res) => {
-    const formattedBody = formatBody(req.body);
+    const sqlObj = humps.decamelizeKeys(req.body);
+    const formattedBody = formatBody(sqlObj);
     const car = await updateCar(req.params.id, formattedBody);
     if (!car.id) {
       res.status(400).json(car.error);
     } else {
-      res.status(200).json(car);
+      const jsObj = humps.camelizeKeys(car);
+      res.status(200).json(jsObj);
     }
   },
 );
