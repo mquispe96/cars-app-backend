97881d68c338bd6b509f2b2631f678fa6fae184a
mquispe96 <mquispe@pursuit.org>
2024-07-17 13:01:50 -0400

    Repo Boiler Plate

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const carsController = require('./controllers/cars.controller.js');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/cars', carsController);
+
+app.get('/', (req, res) => res.send('Welcome to Cars App!'));
+app.get('*', (req, res) => res.status(404).send('404 Not Found'));
+
+module.exports = app;
